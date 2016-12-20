ac6894e2677180d706869354624c067df443d3f2
yujianbo <745198699@qq.com>
2016-12-20 14:52:07 +0800

    [v2.2] 修复String获取的Bug(原以为String的按照StringIdx里面顺序存储的，后来发现有的APK的String是乱序)

--- src/com/ssca/dex/DexStringParser.java
+++ src/com/ssca/dex/DexStringParser.java
@@ -26,12 +26,18 @@ public class DexStringParser {
 				dis.read(eachStringOff);
 				stringOffList.add(i, ByteUtils.byte2Int(eachStringOff));
 			}
-
-			// read string data according to offset
-			int nextSkip = stringOffList.get(0) - stringSize*4 - 0x70;  //0X70 is the size of header
+			//dis now is in the end of string_ids
+			dis.mark(Integer.MAX_VALUE);
+			int needToSkipFromStart = 0x70 + stringSize*4;
 			List<String> stringList = dex.stringList;
-			dis.skipBytes(nextSkip);
+			
+			// read string data according to offset
+//			int nextSkip = stringOffList.get(0) - stringSize*4 - 0x70;  //0X70 is the size of header
+//			dis.skipBytes(nextSkip);
+			
 			for(int i=0;i<stringSize-1;i++){
+				System.out.print("["+i+"] "+stringOffList.get(i));
+				dis.skipBytes(stringOffList.get(i)-needToSkipFromStart);
 				//get chars count from first uleb128
 				int charCount = Uleb128.readLeb128(dis);
 				//alloc char [] according to count
@@ -40,6 +46,8 @@ public class DexStringParser {
 				String s = MUTF8.decode(dis, out);
 				//store s
 				stringList.add(i, s);
+				System.out.println(":"+s);
+				dis.reset();
 			}
 		}else{
 			System.err.println("dis is null");
